f1a5bffb52d9776e426cba786bcbbfbdd4f00c8b
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-02-02 18:23:19 -0500

    Passed activities specs tests

--- db/activities.js
+++ db/activities.js
@@ -28,20 +28,64 @@ async function getAllActivities() {
   }
 }
 
-async function getActivityById(id) { }
+async function getActivityById(id) {
+  try {
+    const { rows: [activity] } = await client.query(`
+SELECT * FROM activities WHERE id=$1
+`, [id]);
 
-async function getActivityByName(name) { }
+
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+
+}
+
+async function getActivityByName(name) {
+  try {
+    const { rows: [activity] } = await client.query(`
+SELECT * FROM activities WHERE name=$1
+`, [name]);
+
+
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {
   // select and return an array of all activities
 }
 
 async function updateActivity({ id, ...fields }) {
-  // don't try to update the id
-  // do update the name and description
-  // return the updated activity
-}
 
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${key}"=$${index + 1}`
+  ).join(', ');
+
+  try {
+    if (setString.length > 0) {
+
+      const { rows: [activity] } = await client.query(`
+            UPDATE activities
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+          `, Object.values(fields));
+
+      return activity;
+    }
+
+  } catch (error) {
+    throw error;
+
+    // don't try to update the id
+    // do update the name and description
+    // return the updated activity
+  }
+}
 module.exports = {
   getAllActivities,
   getActivityById,
