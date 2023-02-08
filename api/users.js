c1852102c154c44e7a59bb1197de25e59d3c18ba
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-02-08 10:11:59 -0500

    Passed api users tests

--- api/users.js
+++ api/users.js
@@ -1,22 +1,24 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const router = express.Router();
-const { getUserByUsername, createUser, getUser } = require("../db");
+const { getUserByUsername,
+      createUser,
+      getPublicRoutinesByUser,
+      getAllRoutinesByUser } = require("../db");
 const jwt = require('jsonwebtoken');
-const { reset } = require("nodemon");
 const { JWT_SECRET } = process.env;
 
-// router.use((req, res, next) => {
-//       console.log("A request is being made to /users");
-//       next();
-// });
+
 
 // POST /api/users/register
 
 router.post('/register', async (req, res, next) => {
+//Get paramaeters needed for the route from the client
       const { username, password } = req.body;
 
       try {
+
+//Check to see if user exists
             const _user = await getUserByUsername(username);
 
             if (_user) {
@@ -26,7 +28,7 @@ router.post('/register', async (req, res, next) => {
                         name: 'UserExistsError'
                   });
             }
-
+//Check to see if password too short
             if (password.length < 8) {
                   res.send({
                         error: 'PasswordTooShortError',
@@ -34,9 +36,9 @@ router.post('/register', async (req, res, next) => {
                         name: 'Password Too Short!'
                   });
             }
-
+//If checks passed, create user
             const user = await createUser({ username, password });
-
+//Add token, attaching id and username
             const token = jwt.sign({
                   id: user.id,
                   username
@@ -57,10 +59,11 @@ router.post('/login', async (req, res, next) => {
       const { username, password } = req.body;
 
       try {
+//Get the user
             const user = await getUserByUsername(username);
-
+//Check to see if user exists and password entered = existing user password
             if (user && user.password == password) {
-
+//Add token, attaching id and username
                   const token = jwt.sign({
                         id: user.id,
                         username
@@ -86,11 +89,16 @@ router.post('/login', async (req, res, next) => {
 router.get('/me', async (req, res, next) => {
 
       try {
+//The token is stored in the "Authorization" header.  When it was created, 
+//id and username were added.  Grab the token from the header, 
+//split it get the payload section, and decode the payload, 
+//using verify and the secret, to extract the id and username
             if (req.headers.authorization) {
                   const usertoken = req.headers.authorization;
                   const token = usertoken.split(' ');
                   const decoded = jwt.verify(token[1], JWT_SECRET);
-                  console.log(decoded);
+//Send the decoded, logged in user info.  If not logged in, 
+//send an error with the 401 status
                   res.send({
                         id: decoded.id, username: decoded.username
                   });
@@ -98,17 +106,40 @@ router.get('/me', async (req, res, next) => {
                   res.status(401)
                   res.send({
                         error: 'GetMeError', name: '401', message: 'You must be logged in to perform this action'
-
                   });
             }
 
       } catch ({ name, message }) {
             next({ name, message })
       }
-
-
 });
 
 // GET /api/users/:username/routines
+
+router.get('/:username/routines', async (req, res, next) => {
+//Get the parameters from the route to input into the functions
+      const { username } = req.params;
+
+      try {
+//Get the logged in username from the token
+            const usertoken = req.headers.authorization;
+            const token = usertoken.split(' ');
+            const decoded = jwt.verify(token[1], JWT_SECRET);
+//Check if the username from the route matches the logged in username
+//Run the function based whether user is logged in or not
+            if (username === decoded.username) {
+                  const routines = await getAllRoutinesByUser({ username });     
+                  res.send(routines);
+            } else {
+                  const user = await getPublicRoutinesByUser({ username });
+                  res.send(user);
+            }
+
+      } catch ({ name, message }) {
+            next({ name, message })
+      }
+
+});
+
 
 module.exports = router;
